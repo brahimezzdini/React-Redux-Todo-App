6ef72901f9841a080c68da5e92910ef7eb469d1f
unknown <ezzdinib@gmail.com>
2021-07-16 18:09:27 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./css/main.css";
+import DisplayTodos from "./components/DisplayTodos";
+import Todos from "./components/Todos";
+
+import { motion } from "framer-motion";
+function App() {
+  return (
+    <div className="App">
+      <motion.h1
+        initial={{ y: -200 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 0.5 }}
+        whileHover={{ scale: 1.1 }}
+      >
+        Todo App
+        
+      </motion.h1>
+      <motion.h1 style={{textAlign:'center',fontSize:'16px'}}>
+      <a href="https://github.com/brahimezzdini/React-Redux-Todo-App.git">SOURCE CODE ON GITHUB</a>
+      </motion.h1>
+     <br/> 
+      <motion.div
+        initial={{ y: 1000 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 1 }}
+      >
+        <Todos />
+        <DisplayTodos />
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
